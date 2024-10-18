28b830b9d5dbce57e763eba82056941276e19fd7
WebMohit20 <2003mohitkhemani@gmail.com>
2024-10-18 12:25:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+const date = new Date();
+const hr = date.getHours();
+const min = date.getMinutes();
+const sec = date.getSeconds();
+
+let hrHand = document.getElementById("hrHand");
+let minHand = document.getElementById("minHand");
+let secHand = document.getElementById("secHand");
+
+
+
+
+
+
